6137b1294771a87e9d0cfe7f1d2e0b1b58f77125
Adil Zhapar <adilzhapar2002@gmail.com>
2023-07-31 18:12:31 +0600

    upd

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import csvParser from 'csv-parser';
 
 dotenv.config();
 
-const scrapeLinkedin = async (url) => {
+const scrapeLinkedin = async (links) => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: ['--disable-popup-blocking'] 
     });
 
@@ -17,7 +19,7 @@ const scrapeLinkedin = async (url) => {
         await page.setViewport({ width: 1280, height: 1080 });
         page.setDefaultNavigationTimeout(0);
 
-        await page.goto(url, { waitUntil: waitUntilOptions });
+        await page.goto(links[0], { waitUntil: waitUntilOptions });
         
         await page.click('.authwall-join-form__form-toggle--bottom');
         
@@ -30,12 +32,32 @@ const scrapeLinkedin = async (url) => {
         await page.waitForNavigation({ waitUntil: waitUntilOptions });
         
         // await page.screenshot({ path: 'example.png' });
-        await page.goto(url, { waitUntil: waitUntilOptions });
-        
-        let workPlace = await page.$eval('.pv-text-details__right-panel-item-text', el => el.children[0].innerHTML.trim());
-        let position = await page.$eval('.text-body-medium', el => el.innerHTML.trim());
-        console.log(workPlace);
-        console.log(position);
+        const data = [];
+        for(let link = 0; link < 10; link++){
+            let workPlace = '';
+            let position = '';
+            try{
+                await page.goto(links[link]);
+
+                workPlace = await page.$eval('.pvs-entity', el=> el.innerHTML);
+            
+                // workPlace = await page.$eval('.pv-text-details__right-panel-item-text', el => el.children[0].innerHTML.trim());
+                // position = await page.$eval('.text-body-medium', el => el.innerHTML.trim());
+            } catch (error) {
+                console.log(links[link], error);
+            }
+            data.push([links[link], workPlace]);
+        }
+
+        const csvData = data.map(row => row.join(',')).join('\n');
+        fs.writeFile('./output.csv', csvData, (err) => {
+            if (err) {
+              console.error('Error writing to the file:', err);
+            } else {
+              console.log('Data has been written to the CSV file successfully.');
+            }
+          });
+
     } catch (error) {
         console.log(error);
     } finally {
@@ -44,9 +66,25 @@ const scrapeLinkedin = async (url) => {
      await browser.close();
 }
 
-const getFilteredLinks = () => {
-    
+const getFilteredLinks = async () => {
+    return new Promise((resolve, reject) => {
+        let emails = []
+        fs.createReadStream('emails.csv')
+            .pipe(csvParser())
+            .on('data', (row) => {
+                emails.push(Object.values(row)[0]);
+            })
+            .on('end', () => {
+                resolve(emails)
+            })
+            .on('error', (err) => {
+                console.log(err);
+            });
+
+        return emails;
+    });
 }
 
-links = getFilteredLinks();
+let links = await getFilteredLinks();
+
 scrapeLinkedin(links);
